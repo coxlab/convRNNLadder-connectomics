c1539b1c073628ea7bed01bbd53cfe5ceb09b8da
Bill Lotter <blotter23@gmail.com>
2016-04-11 16:57:59 -0400

    added param overrides

--- tests/plot_model.py
+++ tests/plot_model.py
@@ -5,6 +5,6 @@ sys.path.append('/home/bill/git_repos/convRNNLadder-connectomics/')
 from config import Config
 from keras_model import *
 
-config = Config({'n_modules': 2, 'nt_in': 2})
+config = Config({'n_modules': 2, 'nt_in': 2, 't_predict': [1]})
 model = ConvLSTMLadderNet(config, build=False)
 plot_model(model)
